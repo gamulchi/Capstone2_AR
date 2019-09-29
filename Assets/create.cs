aac0198760e5cdd35069ed6cc9d4a60b31bdbad3
gamulchi <32320763+gamulchi@users.noreply.github.com>
2019-09-30 01:54:02 +0900

    씬 스크립트,UI 일부 수정

---
+++ Assets/create.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//임시class
+namespace ex
+{
+    public class create : MonoBehaviour
+    {
+        public static int count = 0;
+        // Start is called before the first frame update
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.transform.tag == "ufo")
+            {
+                Destroy(GameObject.FindGameObjectWithTag("ufo"), 0f); //충돌시 ufo 파괴
+                count++; //퀴즈 조건 변수
+            }
+        }
+
+
+    }
+}
