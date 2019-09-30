f1b0990038ee2c49731a95f3fe1f89e0e83761f2
gamulchi <32320763+gamulchi@users.noreply.github.com>
2019-09-30 19:50:38 +0900

    합본

---
+++ Assets/Scripts/Ufo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ufo : MonoBehaviour
+{
+    public string UfoAnswer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            //이펙트 불러오는 스크립트 필요
+            GameObject.FindGameObjectWithTag("Canvas").GetComponent<QuizCSVParser>().KillingUfoCount++;
+            Destroy(this.gameObject);
+        }
+    }
+}
