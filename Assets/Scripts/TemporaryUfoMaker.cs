3aac48615b39457ea793feadd46bf9a50f5eb559
gamulchi <32320763+gamulchi@users.noreply.github.com>
2019-10-01 18:49:56 +0900

    뷰포리아 재설정 및
    뷰포리아 재설정 및 UFO 큐브 속도 설정

---
+++ Assets/Scripts/TemporaryUfoMaker.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryUfoMaker : MonoBehaviour
+{
+    public float StartHeight;
+    public List<Vector2> UfoSpawnList=new List<Vector2>();
+    public GameObject UFOprefab;
+    public float SpawnTime = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartHeight = this.gameObject.transform.position.y;
+        UfoSpawnList.Clear();
+        for(int i = 0; i < GameObject.FindGameObjectsWithTag("UfoSpawnSpot").Length; i++)
+        {
+            Vector2 Vec = new Vector2(GameObject.FindGameObjectsWithTag("UfoSpawnSpot")[i].transform.position.x, GameObject.FindGameObjectsWithTag("UfoSpawnSpot")[i].transform.position.z);
+            UfoSpawnList.Add(Vec);
+        }
+        if (!UFOprefab)
+        {
+            UFOprefab = Resources.Load("TemporaryUfo") as GameObject;
+        }
+        StartCoroutine(MakeUFO());
+    }
+    IEnumerator MakeUFO()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            for(int i = 0; i < UfoSpawnList.Count; i++)
+            {
+                Instantiate(UFOprefab, new Vector3(UfoSpawnList[i].x, StartHeight - 2, UfoSpawnList[i].y),Quaternion.Euler(0,0,0));
+            }
+            yield return new WaitForSeconds(SpawnTime);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UFOMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOMove : MonoBehaviour
+{
+    public float Speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    void Move()
+    {
+        transform.Translate(Vector3.down * Speed * Time.deltaTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
