7cf2af2190fda98f975e4c8402738500f2ec6477
gamulchi <32320763+gamulchi@users.noreply.github.com>
2019-10-04 15:30:02 +0900

    .

---
+++ Assets/Scripts/UFOMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOMove : MonoBehaviour
+{
+    public float Speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if((transform.position.y)%1>0|| (transform.position.z) % 1 > 0)
+        {
+            print("소수소수");
+
+            //0으로 나누었을 때 나머지가 0 초과=>소수
+            int y = (int)gameObject.transform.position.y;
+            int z = (int)gameObject.transform.position.z;
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, y, z);
+        }
+    }
+    void Move()
+    {
+        transform.Translate(Vector3.down * Speed * Time.deltaTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            print("Ufo Meets Ground "+  this.gameObject.name);
+            Destroy(this.gameObject);
+        }
+        if (other.gameObject.tag == "Bullet")
+        {
+            GameObject.FindGameObjectWithTag("Canvas").GetComponent<QuizCSVParser>().KillingUfoCount++;
+            Destroy(this.gameObject);
+        }
+    }
+   
+
+}
