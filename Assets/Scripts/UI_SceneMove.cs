39ee65dfc0bf49e1727522df37a706750ba39344
lunen <lunen 32320739+lunen@users.noreply.github.com>
2019-10-06 16:33:12 +0900

    UI추가

---
+++ Assets/Scripts/UI_SceneMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class UI_SceneMove : MonoBehaviour
+{
+    public GameObject PauseUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PauseUI)
+        {
+            PauseUI.SetActive(false);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Game_Start()
+    {
+        SceneManager.LoadScene("02.PlayScene");
+        //if (PlayScene.name!="")
+        //{
+        //    SceneManager.LoadScene(PlayScene.name);
+
+        //}
+    }
+    public void Game_Tutorial()
+    {
+        SceneManager.LoadScene("03.TutorialScene");
+    }
+    public void Game_Pause()
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Game_Resume()
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void Game_Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("02.PlayScene");
+    }
+    public void Game_Menu()
+    {
+        SceneManager.LoadScene("01.MainScene");
+        Time.timeScale = 1f;
+    }
+    public void Game_Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/quiz1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+//using ex; //전역변수 사용할 namespace
+
+public class quiz1 : MonoBehaviour
+{
+    public GameObject quizz;
+    public GameObject topquiz1;
+    private float time;
+    // Start is called before the first frame update
+
+    void Update()
+    {
+        
+        //if(count == 5)  전역변수 count 퀴즈 호출 조건
+    //   {
+            quizz.SetActive(true);
+    //   }
+       
+        time += Time.deltaTime;
+        if (time == 4)
+        {
+            quizz.SetActive(false);
+            topquiz1.SetActive(true);
+        }
+    }
+}
