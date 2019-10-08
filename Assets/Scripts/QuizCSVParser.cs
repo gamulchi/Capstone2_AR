809a4cee227bd294ebff4b02f78f6558b39ebbf3
gamulchi <32320763+gamulchi@users.noreply.github.com>
2019-10-08 14:39:36 +0900

    퀴즈 비활성화까지

--- Assets/Scripts/QuizCSVParser.cs
+++ Assets/Scripts/QuizCSVParser.cs
@@ -12,6 +12,7 @@ public class QuizCSVParser : MonoBehaviour
     public GameObject QuizIndex;
     public GameObject QuizBackground;
     public List<GameObject> AnswerButtons = new List<GameObject>();
+    public GameObject RightAnswerButton;
     #endregion
     #region 퀴즈 CSV 파싱
     public string AllText;
@@ -19,41 +20,47 @@ public class QuizCSVParser : MonoBehaviour
     public int CSVWidth;
     public string[,] data;
     public int x = 0;
-    public string RightAnswer;
     
     public List<QUIZ> QuizList = new List<QUIZ>();
     public int KillingUfoCount = 0;
     public enum PlayState { NoQuiz,QuizStarted,QuizFinished}
     public PlayState CurrentPlayState=PlayState.NoQuiz;
     #endregion
+    public GameObject UFOManager;
     IEnumerator QuizUIManager()
     {
         while (true)
         {
             //while 쓸때는 EndofFrame()쓰기->while개많이 먹음
+            
             yield return new WaitForEndOfFrame();
             //처음 startcorroutine으로 불러올때 while문 조건과 안맞으면 update에서 조건 맞아도 실행 안됨
+
+            #region 퀴즈 나타나게 하는 함수
             if (KillingUfoCount == 5 && CurrentPlayState == PlayState.NoQuiz)
             {
+                print("QUIZ START");    //->한번만 들어감
+                if (UFOManager.GetComponent<TemporaryUfoMaker>().CanUfoMake == true)
+                {
+                    UFOManager.GetComponent<TemporaryUfoMaker>().CanUfoMake = false;
+                }
+
                 int QuizNum;
                 //중간에 killingUFOCount 값 바뀌어도 한번 들어갔으므로 if문 전체 돌아감
-                #region 퀴즈 몇번째인지 알아내는 함수
+                #region 퀴즈 몇번째인지 알아내고 인덱스 새로 고침해주는(더해주는) 부분
                 string pattern = @"^[0-9]*$";
                 //정규 표현식
-                print("string made");
                 if (System.Text.RegularExpressions.Regex.IsMatch(QuizIndex.GetComponent<TextMeshProUGUI>().text, pattern) == false)
                 {
-                    print("Ismactch check1");
                     QuizIndex.GetComponent<TextMeshProUGUI>().text = "1번째 퀴즈";
                     //만약 퀴즈 인덱스에 숫자가 없다면->게임 플레이 초반=아직 퀴즈 한번도 안나왔음
 
                 }
+                
                 if (System.Text.RegularExpressions.Regex.IsMatch(QuizIndex.GetComponent<TextMeshProUGUI>().text, pattern) == true)
                 {
-                    print("Ismactch check2");
                     QuizNum = Convert.ToInt32(QuizIndex.GetComponent<TextMeshProUGUI>().text.Substring(0, 1));
                     QuizNum++;                        
-                    print("quiznum" + QuizNum);
                 }
                 #endregion
                 int randNum = UnityEngine.Random.Range(0, QuizList.Count);
@@ -68,16 +75,19 @@ public class QuizCSVParser : MonoBehaviour
                 for (int i = 0; i < 3; i++)
                 {
                     int randrand = UnityEngine.Random.Range(0, ar.Count);
-                    print("ran" + randrand);
                     randar.Add(ar[randrand]);
-                    print("randar" + "[" + i + "]:" + randar[i] + " ar[" + randrand + "]: " + ar[randrand]);
                     ar.RemoveAt(randrand);
                 }
                 
                 AnswerButtons[randar[0]].transform.Find("Text").gameObject.GetComponent<TextMeshProUGUI>().text = QuizList[randNum].Answer;
+                if (!RightAnswerButton)
+                {
+                    print("RIGHT");
+                    RightAnswerButton = AnswerButtons[randar[0]];
+                    //퀴즈 맞추면 rightanswerbutton삭제하는 함수 필요
+                }
                 AnswerButtons[randar[1]].transform.Find("Text").gameObject.GetComponent<TextMeshProUGUI>().text = QuizList[randNum].WrongAnswer1;
                 AnswerButtons[randar[2]].transform.Find("Text").gameObject.GetComponent<TextMeshProUGUI>().text = QuizList[randNum].WrongAnswer2;
-                print("QUIZ UI COrroutine");
                 if (QuizText.activeInHierarchy == false)
                 {
                     QuizText.SetActive(true);
@@ -97,11 +107,35 @@ public class QuizCSVParser : MonoBehaviour
                         AnswerButtons[i].SetActive(true);
                     }
                 }
-                print("active");
-                
                 CurrentPlayState = PlayState.QuizStarted;
 
             }
+            #endregion
+            #region 퀴즈 종료시키는 함수
+            if (CurrentPlayState == PlayState.QuizFinished)
+            {
+                if (KillingUfoCount != 0)
+                {
+                    KillingUfoCount = 0;
+                }
+                if (RightAnswerButton)
+                {
+                    RightAnswerButton = null;
+                }
+                QuizBackground.SetActive(false);
+                QuizText.SetActive(false);
+                QuizIndex.SetActive(false);
+                for(int i = 0; i < AnswerButtons.Count; i++)
+                {
+                    AnswerButtons[i].SetActive(false);
+                }
+                if (UFOManager.GetComponent<TemporaryUfoMaker>().CanUfoMake == false)
+                {
+                    UFOManager.GetComponent<TemporaryUfoMaker>().CanUfoMake = true;
+                }
+                CurrentPlayState = PlayState.NoQuiz;
+            }
+            #endregion
         }
     }
     #region 퀴즈 관리하는 함수
@@ -127,7 +161,10 @@ public class QuizCSVParser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!UFOManager)
+        {
+            UFOManager = GameObject.FindGameObjectWithTag("UFOManager");
+        }
         #region 퀴즈 UI 요소들 찾기
         AnswerButtons.Clear();
         if (!QuizText)
