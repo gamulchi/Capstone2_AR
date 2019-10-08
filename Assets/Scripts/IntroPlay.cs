3bf168815645fa6ea41dbd7d25c076c370bf2b15
lunen <lunen 32320739+lunen@users.noreply.github.com>
2019-10-08 19:33:15 +0900

    인트로

---
+++ Assets/Scripts/IntroPlay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroPlay : MonoBehaviour
+{
+    public GameObject[] Intro_image;
+    int num=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Intro_Play", 0, 1f);
+        Invoke("Cancel",5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                SceneManager.LoadScene("01.MainScene");
+            }
+        }
+    }
+    void Intro_Play()
+    {
+        if (num<4)
+        {
+            Intro_image[num].SetActive(true);
+            num++;
+        }
+    }
+    void Cancel()
+    {
+        CancelInvoke("Intro_Play");
+        SceneManager.LoadScene("01.MainScene");
+    }
+}
