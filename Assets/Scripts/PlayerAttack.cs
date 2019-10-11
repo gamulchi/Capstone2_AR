2894a3a651f836924b2632e6cd76303345c801ac
gamulchi <32320763+gamulchi@users.noreply.github.com>
2019-10-11 19:22:21 +0900

    퀴즈 및 점수 완료

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -13,7 +13,7 @@ public class PlayerAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
@@ -32,30 +32,31 @@ public class PlayerAttack : MonoBehaviour
             }
 
         }
+        
+       
         if (Input.touchCount==1)
+
         {
             Touch touch = Input.GetTouch(0);
-            switch (touch.phase)
-            {
+
+            
+            switch (touch.phase) {
                 //이 switch 문 이 있어야 한번 터치할때 Bullet 여러개 instantiate 안됨
                 case TouchPhase.Ended:
                     if (TimeCheckStart == false)
                     {
-                        if (Bullet && GameObject.FindGameObjectWithTag("Canvas").GetComponent<QuizCSVParser>().CurrentPlayState == QuizCSVParser.PlayState.NoQuiz)
-                        {        //총알 존재&&현재 퀴즈 상태가 없으면 공격 가능
-
-                            Attack();
+                        if (Bullet&&GameObject.FindGameObjectWithTag("Canvas").GetComponent<QuizCSVParser>().CurrentPlayState==QuizCSVParser.PlayState.NoQuiz)
+                        {
+                            Instantiate(Bullet, transform.parent);
                         }
                         TimeCheckStart = true;
                     }
+                   
                     break;
             }
+            
+            
         }
         
-        
-    }
-    public void Attack()
-    {
-        Instantiate(Bullet, this.gameObject.transform.position, this.gameObject.transform.rotation);
     }
 }
